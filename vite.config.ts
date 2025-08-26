06a2a1db81adac6e024e1c89acfde458399388d3
Ilya <raisky.ilya3@yandex.ru>
2025-08-26 20:24:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/nonam00/vue-memory-game',
+  base: '/vue-memory-game/',
   plugins: [vue()],
 })
